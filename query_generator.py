156bf4a8c813b28cf3eb6a122c33ba528c1a7662
krazybrazy19xx <157312990+krazybrazy19xx@users.noreply.github.com>
2024-10-18 14:05:45 +0700

    Create query_generator.py

---
+++ query_generator.py
@@ -0,0 +1,18 @@
+from telethon import TelegramClient
+from telethon.functions import messages
+from telethon.types import InputBotAppShortName, AppWebViewResultUrl
+import urllib.parse
+
+async def generate_query(client, peer: str, bot: str, start_param: str, shortname: str):
+    webapp_response: AppWebViewResultUrl = await client(messages.RequestAppWebViewRequest(
+        peer=peer,
+        app=InputBotAppShortName(bot_id=await client.get_input_entity(bot), short_name=shortname),
+        platform='ios',
+        write_allowed=True,
+        start_param=start_param if start_param else None
+    ))
+
+    query_url = webapp_response.url
+    query = urllib.parse.unquote(string=query_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
+    
+    return query
